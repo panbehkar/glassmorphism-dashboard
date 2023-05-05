29a3aeb4426d9e1674329140417f55156db52d3b
Abolfazl Panbehkar <abolfazl.panbehkar@gmail.com>
2023-05-06 01:58:45 +0200

    refactor: :art: Add prettier and its config

---
+++ .prettierrc.cjs
@@ -0,0 +1,5 @@
+module.exports = {
+  singleQuote: true,
+  arrowParens: 'avoid',
+  proseWrap: 'never',
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,4 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./app2.tsx";
+import { createRoot } from 'react-dom/client';
+import App from './app.tsx';
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+createRoot(document.getElementById('root') as HTMLElement).render(<App />);
